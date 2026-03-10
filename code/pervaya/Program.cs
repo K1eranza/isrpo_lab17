3fa5ba9c36ae69e0d762661401c92031f7e06440
K1eranza <zubrevaevelina20641@gmail.com>
2026-03-10 13:39:42 +0300

    Step 6: while and for loops with flowcharts

---
+++ code/pervaya/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вывод элементов массива");
+        
+        int[] numbers = { 5, 3, 8, 1, 9 };
+        
+        Console.WriteLine("Элементы массива:");
+        
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Элемент [{i}] = {numbers[i]}");
+        }
+        
+        Console.WriteLine("\nВывод с использованием foreach:");
+        foreach (int num in numbers)
+        {
+            Console.Write($"{num} ");
+        }
+        Console.WriteLine();
+        
+        Console.WriteLine("\nКонец программы");
+    }
+}
---
+++ code/vtoraya/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Поиск максимального элемента в массиве");
+        
+        int[] numbers = { 5, 3, 8, 1, 9, 12, 4 };
+        
+        Console.WriteLine("Исходный массив:");
+        foreach (int num in numbers)
+        {
+            Console.Write($"{num} ");
+        }
+        Console.WriteLine("\n");
+        
+        int max = numbers[0]; 
+        
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i]; 
+            }
+        }
+        
+        Console.WriteLine($"Максимальный элемент в массиве: {max}");
+        Console.WriteLine("Конец программы");
+    }
+}
